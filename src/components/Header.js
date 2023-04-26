d6dcd0592d1c9d30c95e12ce79769e86f1406668
Nelson Cheng-Lin <nelsonchenglin@gmail.com>
2023-04-26 16:13:07 -0400

    responsive navbar working

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,56 +1,74 @@
 import React from "react";
+import {
+  Flex,
+  Image,
+  IconButton,
+  useDisclosure,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  VStack,
+  Text,
+  HStack,
+  Button,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 import Logo from "../assets/Logo.svg";
 
 function Header() {
-  const handleClick = () => {
-    return null;
-  };
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <>
-      <div id="nav-container">
-        <div className="nav">
-          <a href="/">
-            <img src={Logo} alt="logo svg" />
-          </a>
-          <nav>
-            <ul>
-              <li>
-                <a href="/" onClick={handleClick}>
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="/about" onClick={handleClick}>
-                  About
-                </a>
-              </li>
-              <li>
-                <a href="/menu" onClick={handleClick}>
-                  Menu
-                </a>
-              </li>
-              <li>
-                <a href="/reservations" onClick={handleClick}>
-                  Reservations
-                </a>
-              </li>
-              <li>
-                <a href="/order" onClick={handleClick}>
-                  Order Online
-                </a>
-              </li>
-              <li>
-                <a href="/login" onClick={handleClick}>
-                  Login
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </>
+    <Flex
+      width="100%"
+      px={{ base: "4", md: "6" }}
+      py={{ base: "3", md: "5" }}
+      align="center"
+      justify="space-between"
+    >
+      <Image src={Logo} alt="logo" />
+
+      {/* Hamburger icon */}
+      <IconButton
+        aria-label="Open navigation menu"
+        icon={<HamburgerIcon />}
+        display={{ base: "block", md: "none" }}
+        onClick={onOpen}
+      />
+
+      {/* Mobile navigation menu */}
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+
+          <VStack spacing="6" align="stretch" py="12" px="4">
+            <Text fontWeight="bold">Menu</Text>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/menu">Menu</a>
+            <a href="/reservations">Reservations</a>
+            <a href="/order-online">Order Online</a>
+
+            {/* Close button */}
+            <Button colorScheme="blue" onClick={onClose}>
+              Close
+            </Button>
+          </VStack>
+        </DrawerContent>
+      </Drawer>
+
+      {/* Desktop navigation menu */}
+      <HStack as="nav" spacing="5" display={{ base: "none", md: "flex" }}>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/menu">Menu</a>
+        <a href="/reservations">Reservations</a>
+        <a href="/order-online">Order Online</a>
+        <a href="/login">Login</a>
+      </HStack>
+    </Flex>
   );
 }
-
 export default Header;
