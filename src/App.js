67435e0ee74985de06ea4ddaf2d79442e2e79e73
Nelson Cheng-Lin <nelsonchenglin@gmail.com>
2023-04-26 11:21:30 -0400

    layout in progress

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import Header from "./components/Header";
+import Landing from "./components/Landing";
+import Specials from "./components/Specials";
+import Testimonials from "./components/Testimonials";
+import About from "./components/About";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App">HomePage</div>;
+  return (
+    <>
+      <Header />
+      <Landing />
+      <Specials />
+      <Testimonials />
+      <About />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
