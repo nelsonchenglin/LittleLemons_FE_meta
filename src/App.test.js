6ee31d629645bb528e6f183e5af2b0a83092a393
Nelson Cheng-Lin <nelsonchenglin@gmail.com>
2023-05-22 14:44:46 -0400

    added simple form testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,63 @@
-import { initializeTimes, updateTimes } from "./components/Reservations";
-import { fetchAPI } from "./api";
+import { render, fireEvent } from "@testing-library/react";
+import Contact from "./components/Contact";
 
-test("initializeTimes should return an array of strings with valid time slots", () => {
-  const expectedTimes = fetchAPI(new Date());
-  expect(initializeTimes()).toEqual(expectedTimes);
+test("Form submits successfully when all fields are filled correctly", () => {
+  const formData = {
+    formName: "John Doe",
+    formEmail: "johndoe@jd.com",
+    message: "Testing testing",
+  };
+
+  const setFormName = jest.fn();
+  const setFormEmail = jest.fn();
+  const setMessage = jest.fn();
+  const alert = jest.spyOn(window, "alert");
+
+  const { getByLabelText, getByText } = render(
+    <Contact
+      setFormName={setFormName}
+      setFormEmail={setFormEmail}
+      setMessage={setMessage}
+    />
+  );
+
+  fireEvent.change(getByLabelText(/Name/i), {
+    target: { value: formData.formName },
+  });
+  fireEvent.change(getByLabelText(/Email address/i), {
+    target: { value: formData.formEmail },
+  });
+  fireEvent.change(getByLabelText(/Message/i), {
+    target: { value: formData.message },
+  });
+
+  fireEvent.click(getByText("Submit"));
+
+  expect(alert).toHaveBeenCalledWith(
+    "Thank you for contacting us, we will get back to you soon!"
+  );
 });
 
-describe("updateTimes", () => {
-  it("should return the same state value when given an unknown action type", () => {
-    const state = ["11:00 AM", "11:30 AM"];
-    const action = { type: "SOME_ACTION" };
-    expect(updateTimes(state, action)).toEqual(state);
-  });
+test("Form displays error message when message field is empty", () => {
+  const setFormName = jest.fn();
+  const setFormEmail = jest.fn();
+  const setMessage = jest.fn();
+  const handleSubmit = jest.fn();
+  const alert = jest.spyOn(window, "alert");
+
+  const { getByText } = render(
+    <Contact
+      setFormName={setFormName}
+      setFormEmail={setFormEmail}
+      setMessage={setMessage}
+    />
+  );
+  fireEvent.click(getByText("Submit"));
+
+  expect(setFormName).not.toHaveBeenCalled();
+  expect(setFormEmail).not.toHaveBeenCalled();
+  expect(setMessage).not.toHaveBeenCalled();
+  expect(handleSubmit).not.toHaveBeenCalled();
+  expect(alert).not.toHaveBeenCalled();
+  expect(getByText("Message field cannot be empty")).toBeInTheDocument();
 });
